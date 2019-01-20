8918c356be117ceb974c0a8aa9fe74e1fe668a50
John Robinson <johnboy@DESKTOP-TK3TEP8.localdomain>
2019-01-20 14:16:48 -0600

    Initial revision:

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+// AgcBarleyProductionCost: CommonJS Main
---
+++ dist/types/components/agc-barley-production-cost-results-placeholder/agc-barley-production-cost-results-placeholder.d.ts
@@ -0,0 +1,4 @@
+import '../../stencil.core';
+export declare class AgcBarleyProductionCostResultsPlaceholder {
+    render(): JSX.Element;
+}
